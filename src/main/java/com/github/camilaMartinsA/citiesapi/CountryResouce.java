d6b018a245c9100ca11b19f93400a26302a44014
camilaMartinsA <camilaalvesmartins19@gmail.com>
2021-11-09 04:08:41 -0300

    Alteração no arquivo

--- src/main/java/com/github/camilaMartinsA/citiesapi/CountryResouce.java
+++ src/main/java/com/github/camilaMartinsA/citiesapi/CountryResouce.java
@@ -1,16 +1,40 @@
 package com.github.camilaMartinsA.citiesapi;
 
 import com.github.camilaMartinsA.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.awt.print.Pageable;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/countryes")
 public class CountryResouce {
+
     private CountryRepository repository;
-    
-@GetMapping
-    public List<Country> contries(){
-        return repository.findAll();
+
+    public CountryResouce(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Page<Country> contries(Pageable page){
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/(id)")
+    public ResponseEntity getOne (@PathVariable long id) {
+        Optional <Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/github/camilaMartinsA/citiesapi/repository/CountryRepository.java
+++ src/main/java/com/github/camilaMartinsA/citiesapi/repository/CountryRepository.java
@@ -1,4 +1,8 @@
 package com.github.camilaMartinsA.citiesapi.repository;
 
-public interface CountryRepository {
+import com.github.camilaMartinsA.citiesapi.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
 }
